78e5614178eda639181c42377f93ffd72c2e97c9
SPREADTEC\alicia.santos <alicia.santos@spread.com.br>
2022-06-10 16:18:57 -0300

    Remove any non number character before format CPF (fixed bug)

--- execute_python_code_scripts/format_valid_cpf.py
+++ execute_python_code_scripts/format_valid_cpf.py
@@ -1,7 +1,18 @@
 import sys
+import re
+
+# Remove dots, traces and any other non number character from cpf
+def remove_non_numbers_characters(cpf):
+    cpf_number = re.sub('[^0-9]', '', cpf)
+
+    return cpf_number
+    
 
 # Get valid CPF's strings and format to correct pattern (000.000.000-00) 
 def format_cpf(cpf):
+
+    # Left only numbers in CPF
+    cpf = remove_non_numbers_characters(cpf)
 
     formated_cpf = f'{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}'
 
