a1a2417b370d18cc9f6eb40810462b212847befa
SPREADTEC\alicia.santos <alicia.santos@spread.com.br>
2022-06-27 11:15:36 -0300

    Let CPF with only numbers

--- execute_python_code_scripts/format_valid_cpf.py
+++ execute_python_code_scripts/format_valid_cpf.py
@@ -8,23 +8,12 @@ def remove_non_numbers_characters(cpf):
     return cpf_number
     
 
-# Get valid CPF's strings and format to correct pattern (000.000.000-00) 
-def format_cpf(cpf):
-
-    # Left only numbers in CPF
-    cpf = remove_non_numbers_characters(cpf)
-
-    formated_cpf = f'{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}'
-
-    return formated_cpf
-
-
 if __name__ == '__main__':
 
     # Get data from ElectroNeek
     input_cpf = sys.argv[1]
 
-    formated_cpf = format_cpf(input_cpf)
+    formated_cpf = remove_non_numbers_characters(input_cpf)
 
     # Return formated cpf to ElectroNeek
     print(formated_cpf)
